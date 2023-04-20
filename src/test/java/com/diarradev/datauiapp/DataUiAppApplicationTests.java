490c0d3fe9129b2abb451caf40c63ba15b0fdf9e
Moise DIARRA <moisepr@gmail.com>
2023-04-20 19:24:40 +0000

    add resources and Java folders

---
+++ src/test/java/com/diarradev/datauiapp/DataUiAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diarradev.datauiapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataUiAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
